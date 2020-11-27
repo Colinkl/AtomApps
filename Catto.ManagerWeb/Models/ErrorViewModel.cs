f1daaec5b98c0afddbe1f628afe7587ed1a49986
Cat Cay <col1nkl@yandex.ru>
2020-11-28 00:00:59 +0700

    Magager mvc

---
+++ Catto.ManagerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Catto.ManagerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
